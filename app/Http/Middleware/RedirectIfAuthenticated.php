e6132757e4dedab9c21ebafe9c81e07924219f8b
Milan Awal <milanawal123@gmail.com>
2024-06-27 23:11:21 +0545

    remove dd

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            dd(auth()->user());
+            // dd(auth()->user());
             return redirect('/home');
         }
 
